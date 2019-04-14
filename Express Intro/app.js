87f61f5bf2338ab40cac39e3577e66100f996bf3
Asel Peiris <peirisasel373@gmail.com>
2019-04-14 23:48:59 +0530

    ExpressIntro

---
+++ Express Intro/app.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('In the middle ware functions')
+    next()
+}); //Allows to add a new middleware function. The function added here will be executed for every incoming request
+
+app.use((req, res, next) => {
+    console.log('In another middle ware functions')
+});
+
+const server = http.createServer(app)
+
+server.listen(3000)
